964f5e81c0ee373d7fd835b0cc53d9367e9eeb91
jrangu <55324369+jrangu@users.noreply.github.com>
2020-04-15 18:15:28 -0700

    Added share button.

--- TopNews/app/src/main/java/com/smartphone/topnews/network/RowAdapter.kt
+++ TopNews/app/src/main/java/com/smartphone/topnews/network/RowAdapter.kt
@@ -1,23 +1,28 @@
 package com.smartphone.topnews.network
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.smartphone.topnews.R
 import kotlinx.android.synthetic.main.row.view.*
 
 
-
-class RowAdapter(private val myDataset: List<NewsArticles>) : RecyclerView.Adapter<RowAdapter.MyViewHolder>(){
+class RowAdapter(private val myDataset: List<NewsArticles>) :
+    RecyclerView.Adapter<RowAdapter.MyViewHolder>() {
 
     var imageUrl: String = ""
 
     class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view)
-    override fun onCreateViewHolder(parent: ViewGroup,
-                                    viewType: Int): RowAdapter.MyViewHolder {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RowAdapter.MyViewHolder {
         // create a new view
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.row, parent, false) as View
@@ -35,7 +40,17 @@ class RowAdapter(private val myDataset: List<NewsArticles>) : RecyclerView.Adapt
         holder.view.cv.setOnClickListener {
 
         }
+        holder.view.btnShare.setOnClickListener {
+            holder.view.label1.text = "testing"
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
+                type = "text/plain"
+            }
 
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            //startActivity(shareIntent)
+        }
 
 
     }
