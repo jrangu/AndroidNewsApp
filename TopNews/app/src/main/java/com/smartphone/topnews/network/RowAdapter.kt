5a9e4b4bbf89f17fd69f96c75cfc9affe302b015
jrangu <55324369+jrangu@users.noreply.github.com>
2020-04-15 23:38:12 -0700

    Image binding in card view.

--- TopNews/app/src/main/java/com/smartphone/topnews/network/RowAdapter.kt
+++ TopNews/app/src/main/java/com/smartphone/topnews/network/RowAdapter.kt
@@ -3,6 +3,8 @@ package com.smartphone.topnews.network
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat.startActivity
@@ -15,7 +17,7 @@ import kotlinx.android.synthetic.main.row.view.*
 class RowAdapter(private val myDataset: List<NewsArticles>) :
     RecyclerView.Adapter<RowAdapter.MyViewHolder>() {
 
-    var imageUrl: String = ""
+    // var imageUrl: String = ""
 
     class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view)
 
@@ -26,7 +28,7 @@ class RowAdapter(private val myDataset: List<NewsArticles>) :
         // create a new view
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.row, parent, false) as View
-        Glide.with(parent.context).load(imageUrl).into(view.imageview2);
+
         // set the view's size, margins, paddings and layout parameters
         return MyViewHolder(view)
     }
@@ -36,15 +38,20 @@ class RowAdapter(private val myDataset: List<NewsArticles>) :
         // - replace the contents of the view with that element
         //holder.view.label1.text = myDataset.get(position).publishedAt
         holder.view.label1.text = myDataset.get(position).title
-        imageUrl = myDataset.get(position).urlToImage.toString()
+        val imageUrl = myDataset.get(position).urlToImage.toString()
+        if (imageUrl == null) {
+            holder.view.imageview2.visibility = INVISIBLE
+        } else {
+            holder.view.imageview2.visibility = VISIBLE
+            Glide.with(holder.view.context).load(imageUrl).into(holder.view.imageview2);
+        }
         holder.view.cv.setOnClickListener {
-
         }
         holder.view.btnShare.setOnClickListener {
             val newsUrl = myDataset.get(position).url
             val sendIntent: Intent = Intent().apply {
                 action = Intent.ACTION_SEND
-                putExtra(Intent.EXTRA_TEXT, newsUrl )
+                putExtra(Intent.EXTRA_TEXT, newsUrl)
                 type = "text/plain"
             }
             val shareIntent = Intent.createChooser(sendIntent, null)
