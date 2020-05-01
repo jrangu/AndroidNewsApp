1ed809b2744ff9ef934dd8845b5e0b4a9e9d66d5
manishayacham <manisha.yacham16@gmail.com>
2020-04-30 23:02:22 -0700

    Code for google firebase login

--- TopNews/app/src/main/java/com/smartphone/topnews/MainActivity.kt
+++ TopNews/app/src/main/java/com/smartphone/topnews/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.smartphone.topnews
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import android.view.View
+import androidx.activity.viewModels
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -13,12 +16,25 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.navigation.NavArgument
-import androidx.navigation.NavType
+import androidx.lifecycle.Observer
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.nav_header_main.view.*
+import com.smartphone.topnews.Firebase.LoginViewModel
+
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private val loginviewModel by viewModels<LoginViewModel>()
+    private lateinit var headerView: View
+
+    companion object {
+        const val TAG = "MainActivity"
+        const val SIGN_IN_RESULT_CODE = 1001
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,12 +45,16 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        headerView = navView.getHeaderView(0)
+        observeAuthenticationState()
+        headerView.loginbutton.setOnClickListener{launchSignInFlow()}
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -46,5 +66,65 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun launchSignInFlow() {
+        // Give users the option to sign in / register with their email or Google account. If users
+        // choose to register with their email, they will need to create a password as well.
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        // Create and launch sign-in intent. We listen to the response of this activity with the
+        // SIGN_IN_RESULT_CODE code.
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
+                providers
+            ).build(), SIGN_IN_RESULT_CODE
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_IN_RESULT_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in user.
+                Log.i(
+                    TAG,
+                    "Successfully signed in user " +
+                            "${FirebaseAuth.getInstance().currentUser?.displayName}!"
+                )
+            } else {
+                // Sign in failed. If response is null the user canceled the sign-in flow using
+                // the back button. Otherwise check response.getError().getErrorCode() and handle
+                // the error.
+                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+            }
+        }
+    }
+
+    private fun observeAuthenticationState() {
+
+        loginviewModel.authenticationState.observe(this, Observer { authenticationState ->
+            when (authenticationState) {
+                LoginViewModel.AuthenticationState.AUTHENTICATED -> {
+                    headerView.navheadertextView.text = loginviewModel.getUserInfo()
+                    headerView.loginbutton.text = "SignOut"
+                    headerView.loginbutton.setOnClickListener{
+                        AuthUI.getInstance().signOut(this).addOnCompleteListener {
+                            Log.i(TAG, "Sign out successful")
+                        }
+                    }
+                }
+                else -> {
+                    headerView.navheadertextView.text = "No Account"
+                    headerView.loginbutton.text = "SignIn"
+                    headerView.loginbutton.setOnClickListener{
+                        launchSignInFlow()
+                    }
+                }
+            }
+        })
     }
 }
