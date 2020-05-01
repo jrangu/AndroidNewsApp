c20729b1e98ded51e9f1b55a3ec17490b473de70
manishayacham <manisha.yacham16@gmail.com>
2020-04-30 23:07:40 -0700

    Changes

--- TopNews/app/src/main/java/com/smartphone/topnews/MainActivity.kt
+++ TopNews/app/src/main/java/com/smartphone/topnews/MainActivity.kt
@@ -7,6 +7,9 @@ import android.util.Log
 import android.view.Menu
 import android.view.View
 import androidx.activity.viewModels
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
